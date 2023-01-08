8273842f5f29409710518659f5337fb45698ef63
Muhammadekoajisaputro <122192123+Muhammadekoajisaputro@users.noreply.github.com>
2023-01-08 12:20:04 +0700

    Initial Commit

---
+++ CodeScaningTest/CodeScaningTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScaningTestApp.swift
+//  CodeScaningTest
+//
+//  Created by Aji on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeScaningTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
